aad2ed4b574aa1f9f26015b4fe73fba92cd84549
dkrupkina <dkrupkina@solanteq.com>
2023-01-31 02:09:23 +0300

    Enhance PatrialUpdateTest * New tests * Fix naming

--- src/test/java/com/booking/test/PartialUpdateTest.kt
+++ src/test/java/com/booking/test/PartialUpdateTest.kt
@@ -10,14 +10,15 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
+
 @DisplayName("PartialUpdateBooking")
 class PartialUpdateTest : BookingApiTest() {
 
     @Nested
-    @DisplayName("Update and check all fields")
-    inner class checkAllFields {
+    @DisplayName("Update only what is requested")
+    inner class UpdateRequested {
         @Test
-        fun `Lastname should be changed, other fields unchangeable`() {
+        fun `Should update single field when lastname is updated`() {
             val id = BookingApi.create(
                 booking(
                     lastName = "Bb",
@@ -46,7 +47,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Deposit and additional need should be changed, other fields unchangeable`() {
+        fun `Should only update 2 fields when only depositpaid and additionalneeds are updated`() {
             val id = BookingApi.create(
                 booking(
                     lastName = "Bb",
@@ -76,7 +77,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Nothing should be changed after update with empty body`() {
+        fun `Should not update any fields on empty body`() {
             val id = BookingApi.create(
                 booking(
                     lastName = "Bb",
@@ -105,7 +106,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Every field should be changed`() {
+        fun `Should update every field when everything is updated`() {
             val id = BookingApi.create(
                 booking(
                     lastName = "Bb", firstName = "Joey", totalPrice = 252,
@@ -140,122 +141,62 @@ class PartialUpdateTest : BookingApiTest() {
         }
     }
 
-    @Test
-    fun `Checkin only should be changed`() {
-        val id = BookingApi.create(
-            booking(
-                bookingDates = BookingDates(
-                    LocalDate.of(2011, 12, 30),
-                    LocalDate.of(2011, 12, 31)
-                ),
-                depositPaid = false
-            )
-        )
-        val rq = PartialBooking(
-            bookingDates = PartialBookingDates(
-                checkIn = LocalDate.of(2011, 12, 27)
-            )
-        )
+    @Nested
+    @DisplayName("Update date")
+    inner class UpdateDate {
 
-        val response = bookingApi.partialUpdate(id, rq)
-
-        assertEquals(HttpStatus.SC_OK, response.statusCode)
-        val body = response.jsonPath()
-        assertEquals("2011-12-27", body.get("bookingdates.checkin"))
-        assertEquals("2011-12-31", body.get("bookingdates.checkout"))
-    }
-
-    @Test
-    fun `Should be changed with checkin bigger than checkout`() {
-        val id = BookingApi.create(
-            booking(
-                bookingDates = BookingDates(
-                    LocalDate.of(2011, 12, 30),
-                    LocalDate.of(2011, 12, 31)
+        @Test
+        fun `Should update single date`() {
+            val id = BookingApi.create(
+                booking(
+                    bookingDates = BookingDates(
+                        LocalDate.of(2011, 12, 30),
+                        LocalDate.of(2011, 12, 31)
+                    ),
+                    depositPaid = false
                 )
             )
-        )
-        val rq = PartialBooking(
-            bookingDates = PartialBookingDates(
-                LocalDate.of(2012, 1, 30),
-                LocalDate.of(2012, 1, 15)
+            val rq = PartialBooking(
+                bookingDates = PartialBookingDates(
+                    checkIn = LocalDate.of(2011, 12, 27)
+                )
             )
-        )
-
-        val response = bookingApi.partialUpdate(id, rq)
-
-        assertEquals(HttpStatus.SC_OK, response.statusCode)
-        val body = response.jsonPath()
-        assertEquals("2012-01-30", body.get("bookingdates.checkin"))
-        assertEquals("2012-01-15", body.get("bookingdates.checkout"))
-    }
-
-    @Nested
-    @DisplayName("Incorrect format")
-    inner class IncorrectFormat {
-        @Test
-        fun `Should be successful with non-existing field, not applied`() {
-            val id = BookingApi.create(booking())
-            val rq = mapOf("depositamount" to 200)
-
-            val response = bookingApi.partialUpdatePlain(id, rq)
-
-            assertEquals(HttpStatus.SC_OK, response.statusCode)
-            assertNull(response.jsonPath().get("depositamount"))
-        }
-
-        @Test
-        fun `Should be successful with Double in amount of totalPrice`() {
-            val id = BookingApi.create(booking(totalPrice = 250))
-            val rq = mapOf("totalprice" to "220.50")
-
-            val response = bookingApi.partialUpdatePlain(id, rq)
-
-            assertEquals(HttpStatus.SC_OK, response.statusCode)
-            assertEquals(220, response.jsonPath().getLong("totalprice"))
-        }
-
-        @Test
-        fun `Should be successful with negative amount of totalPrice`() {
-            val id = BookingApi.create(booking(totalPrice = 250))
-            val rq = PartialBooking(totalPrice = -100)
 
             val response = bookingApi.partialUpdate(id, rq)
 
             assertEquals(HttpStatus.SC_OK, response.statusCode)
-            assertEquals(-100, response.jsonPath().getLong("totalprice"))
+            val body = response.jsonPath()
+            assertEquals("2011-12-27", body.get("bookingdates.checkin"))
+            assertEquals("2011-12-31", body.get("bookingdates.checkout"))
         }
 
         @Test
-        fun `Should be successful with String in depositPaid`() {
-            val id = BookingApi.create(booking(depositPaid = false))
-            val rq = mapOf("depositpaid" to "true")
-
-            val response = bookingApi.partialUpdatePlain(id, rq)
-
-            assertEquals(HttpStatus.SC_OK, response.statusCode)
-            assertTrue(response.jsonPath().getBoolean("depositpaid"))
-        }
-
-        @Test
-        fun `Should be successful with incorrect month and day values`() {
-            val id = BookingApi.create(booking())
-            val rq = mapOf(
-                "bookingdates" to mapOf(
-                    "checkin" to "2012-33-93",
-                    "checkout" to "2012-44-83"
+        fun `Should update when checkin bigger than checkout`() {
+            val id = BookingApi.create(
+                booking(
+                    bookingDates = BookingDates(
+                        LocalDate.of(2011, 12, 30),
+                        LocalDate.of(2011, 12, 31)
+                    )
+                )
+            )
+            val rq = PartialBooking(
+                bookingDates = PartialBookingDates(
+                    LocalDate.of(2012, 1, 30),
+                    LocalDate.of(2012, 1, 15)
                 )
             )
 
-            val response = bookingApi.partialUpdatePlain(id, rq)
+            val response = bookingApi.partialUpdate(id, rq)
 
             assertEquals(HttpStatus.SC_OK, response.statusCode)
-            assertEquals("0NaN-aN-aN", response.jsonPath().get("bookingdates.checkin"))
-            assertEquals("0NaN-aN-aN", response.jsonPath().get("bookingdates.checkout"))
+            val body = response.jsonPath()
+            assertEquals("2012-01-30", body.get("bookingdates.checkin"))
+            assertEquals("2012-01-15", body.get("bookingdates.checkout"))
         }
 
         @Test
-        fun `Should be successful with incorrect order in date format`() {
+        fun `Should not update when incorrect order in date format`() {
             val id = BookingApi.create(booking())
             val rq = mapOf(
                 "bookingdates" to mapOf(
@@ -272,7 +213,24 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Should be successful with incorrect date format`() {
+        fun `Should break state when incorrect month and day values`() {
+            val id = BookingApi.create(booking())
+            val rq = mapOf(
+                "bookingdates" to mapOf(
+                    "checkin" to "2012-33-93",
+                    "checkout" to "2012-44-83"
+                )
+            )
+
+            val response = bookingApi.partialUpdatePlain(id, rq)
+
+            assertEquals(HttpStatus.SC_OK, response.statusCode)
+            assertEquals("0NaN-aN-aN", response.jsonPath().get("bookingdates.checkin"))
+            assertEquals("0NaN-aN-aN", response.jsonPath().get("bookingdates.checkout"))
+        }
+
+        @Test
+        fun `Should break state when incorrect date format`() {
             val id = BookingApi.create(booking())
             val rq = mapOf(
                 "bookingdates" to mapOf(
@@ -287,9 +245,56 @@ class PartialUpdateTest : BookingApiTest() {
             assertEquals("0NaN-aN-aN", response.jsonPath().get("bookingdates.checkin"))
             assertEquals("0NaN-aN-aN", response.jsonPath().get("bookingdates.checkout"))
         }
+    }
+
+    @Nested
+    @DisplayName("Invalid request")
+    inner class InvalidRequest {
+        @Test
+        fun `Should not add non-existing field`() {
+            val id = BookingApi.create(booking())
+            val rq = mapOf("depositamount" to 200)
+
+            val response = bookingApi.partialUpdatePlain(id, rq)
+
+            assertEquals(HttpStatus.SC_OK, response.statusCode)
+            assertNull(response.jsonPath().get("depositamount"))
+        }
 
         @Test
-        fun `Should be successful with duplicate field, last applied`() {
+        fun `Should succeed with Double in totalprice`() {
+            val id = BookingApi.create(booking(totalPrice = 250))
+            val rq = mapOf("totalprice" to "220.50")
+
+            val response = bookingApi.partialUpdatePlain(id, rq)
+
+            assertEquals(HttpStatus.SC_OK, response.statusCode)
+        }
+
+        @Test
+        fun `Should update when negative totalprice`() {
+            val id = BookingApi.create(booking(totalPrice = 250))
+            val rq = PartialBooking(totalPrice = -100)
+
+            val response = bookingApi.partialUpdate(id, rq)
+
+            assertEquals(HttpStatus.SC_OK, response.statusCode)
+            assertEquals(-100, response.jsonPath().getLong("totalprice"))
+        }
+
+        @Test
+        fun `Should succeed when String in depositpaid`() {
+            val id = BookingApi.create(booking(depositPaid = false))
+            val rq = mapOf("depositpaid" to "true")
+
+            val response = bookingApi.partialUpdatePlain(id, rq)
+
+            assertEquals(HttpStatus.SC_OK, response.statusCode)
+            assertTrue(response.jsonPath().getBoolean("depositpaid"))
+        }
+
+        @Test
+        fun `Should update with last value on duplicated field`() {
             val id = BookingApi.create(booking(lastName = "Aaa"))
             val rq = mapOf("lastname" to "Brown", "lastname" to "Tribiani")
 
@@ -300,7 +305,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Should be forbidden without cookie`() {
+        fun `Should be forbidden when no token`() {
             val id = BookingApi.create(booking())
 
             val response = bookingApi.partialUpdate(id, simplePartialRq(), null)
@@ -309,7 +314,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Should be not allowed by negative id`() {
+        fun `Should be error when id is negative`() {
             val id = BookingApi.create(booking())
 
             val response = bookingApi.partialUpdate("-$id", simplePartialRq())
@@ -318,7 +323,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Should be not allowed by non-existing id`() {
+        fun `Should be not allowed on non-existing id`() {
             val id = BookingApi.create(booking())
 
             bookingApi.delete(id)
@@ -329,7 +334,7 @@ class PartialUpdateTest : BookingApiTest() {
         }
 
         @Test
-        fun `Should be not allowed by String instead of id`() {
+        fun `Should be not allowed on random string instead of id`() {
             val response = bookingApi.partialUpdatePlain("acc", mapOf("additionalNeeds" to ""))
 
             assertEquals(HttpStatus.SC_METHOD_NOT_ALLOWED, response.statusCode)
