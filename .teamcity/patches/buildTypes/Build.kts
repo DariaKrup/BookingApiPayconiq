929b3824262e5fd739212c9e89e8a1c666e1325e
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2023-07-07 11:34:56 +0200

    Update Build.kts

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -22,7 +22,7 @@ changeBuildType(RelativeId("Build")) {
             password("password.param", "credentialsJSON:c471b542-ee2a-49d0-8361-fb34ffec62c2")
         }
         add {
-            param("parameter_for_dsl", "")
+            param("parameter_for_dsl", "${DslContext.getParameter("serverUrl", "default")}")
         }
         add {
             param("env.JDK_17_0_x64", "%env.JRE_HOME%")
