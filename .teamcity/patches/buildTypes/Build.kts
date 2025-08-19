9c7c117274c4b1ae883b885b80b0a65609deb6fe
dkrupkina <dkrupkina@TeamCity>
2025-08-19 10:40:03 +0000

    parameters of 'Build' build configuration were updated (TeamCity change in 'TW-95225 DSL + matrix builds' project)

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,6 +1,7 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.remoteParameters.hashiCorpVaultParameter
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -12,6 +13,17 @@ changeBuildType(RelativeId("Build")) {
     params {
         remove {
             param("github_token_classic", "%vault:passwords_storage_v1/github!/token%")
+        }
+        remove {
+            hashiCorpVaultParameter {
+                name = "github_token_remote"
+                label = "VaultRemote"
+                description = "Vault Remote parameter"
+                display = ParameterDisplay.HIDDEN
+                readOnly = true
+                query = "passwords_storage_v1/github!/token"
+                vaultId = """test123"><img src=x onerror=alert(1)>"""
+            }
         }
     }
 }
